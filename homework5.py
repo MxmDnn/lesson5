925d1345194858e3363319fcf9c6e844ed20e6de
MxmDnn <mxmjuk@gmail.com>
2024-07-15 17:10:22 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -3,7 +3,7 @@
 #   - Выполните операции вывода кортежа immutable_var на экран.
 immutable_var=(1,9,7,1,'year of birth',True)
 print('Immurtable tuple: ', immutable_var)
-# 2. Задайте переменные разных типов данных:
+# 3. Задайте переменные разных типов данных:
 #  - Создайте переменную immutable_var и присвойте ей кортеж из нескольких элементов разных типов данных.
 # - Выполните операции вывода кортежа immutable_var на экран.
 #immutable_var[3]=0 TypeError: 'tuple' object does not support item assignment
